f2bde1a6664ff9efb225a6d465ff8a9fa3d4a296
BrickNumber5 <79104486+BrickNumber5@users.noreply.github.com>
2021-12-16 20:57:58 -0700

    Added Fractal, Misc Work

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const fcnv = document.querySelector( ".front" );
 const fctx = fcnv.getContext( "2d" );
 
+const bcnv = document.querySelector( ".back" );
+const bctx = bcnv.getContext( "2d" );
+
 let width, height;
 
+let l_sec;
 function draw( ) {
+  //Replatce new Date( ) with new Date((+(new Date( ))*1000)%(1000*60*60*24)) for hyperspeed
+  const date = new Date( );
+  const n_sec = date.getSeconds( );
+  if ( n_sec === l_sec ) {
+    requestAnimationFrame( draw );
+    return;
+  }
+  l_sec = n_sec;
+  
   fctx.clearRect( 0, 0, width, height );
   
   const r = Math.min( width, height ) / 4;
@@ -21,9 +34,6 @@ function draw( ) {
   fctx.beginPath( );
   fctx.arc( cx, cy, r, 0, 2 * Math.PI );
   fctx.stroke( );
-  
-  const date = new Date( );
-  
   
   const second_angle = 2 * Math.PI * date.getSeconds( ) / 60;
   const minute_angle = 2 * Math.PI * date.getMinutes( ) / 60 + second_angle / 60;
@@ -68,14 +78,52 @@ function draw( ) {
   fctx.lineTo( cx, cy );
   fctx.stroke( );
   
+  // Draw Fractal
+  bctx.clearRect( 0, 0, width, height );
+  bctx.lineCap = "round";
+  drawFractalClock( { hour_angle, minute_angle, r, s }, 0, cx, cy, 0 );
+  
   requestAnimationFrame( draw );
 }
 
 requestAnimationFrame( draw );
 
+function drawFractalClock( consts, scale, sx, sy, angle ) {
+  const { hour_angle, minute_angle, r, s } = consts;
+  const w = s * 1 / ( 1.3 ** scale );
+  if ( w < 1 ) return;
+  
+  const ha = angle + hour_angle;
+  const [ hx, hy ] = [
+    sx + r * 0.85 * Math.cos( ha - Math.PI / 2 ) / ( 1.5 ** scale ),
+    sy + r * 0.85 * Math.sin( ha - Math.PI / 2 ) / ( 1.5 ** scale )
+  ];
+  
+  const ma = angle + minute_angle;
+  const [ mx, my ] = [
+    sx + r * 0.85 * Math.cos( ma - Math.PI / 2 ) / ( 1.5 ** scale ),
+    sy + r * 0.85 * Math.sin( ma - Math.PI / 2 ) / ( 1.5 ** scale )
+  ];
+  
+  drawFractalClock( consts, scale + 1, hx, hy, ha );
+  drawFractalClock( consts, scale + 1, mx, my, ma );
+  
+  bctx.strokeStyle = `hsl(${ hour_angle }rad 100% 50%)`;
+  bctx.lineWidth = w;
+  bctx.beginPath( );
+  bctx.moveTo( sx, sy );
+  bctx.lineTo( hx, hy );
+  bctx.stroke( );
+  
+  bctx.beginPath( );
+  bctx.moveTo( sx, sy );
+  bctx.lineTo( mx, my );
+  bctx.stroke( );
+}
+
 function onresize( ) {
-  width = fcnv.width = window.innerWidth;
-  height = fcnv.height = window.innerHeight;
+  width = bcnv.width = fcnv.width = window.innerWidth;
+  height = bcnv.height = fcnv.height = window.innerHeight;
 }
 window.onresize = onresize;
 onresize( );
